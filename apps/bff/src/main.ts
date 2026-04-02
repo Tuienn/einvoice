49cd9b1a13c2e21f6c169bf432458fd966d0d12b
Tuienn <tuyenboy1234@gmail.com>
2026-04-02 17:01:06 +0700

    chore/#swagger: Init swager

--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -1,20 +1,35 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common'
+import { Logger, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app/app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     const globalPrefix = AppModule.CONFIGURATION.GLOBAL_PREFIX
     app.setGlobalPrefix(globalPrefix)
-    const port = AppModule.CONFIGURATION.PORT
+    app.useGlobalPipes(new ValidationPipe({ transform: true }))
 
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('BFF API')
+        .setDescription('BFF API documentation')
+        .setVersion('1.0')
+        .addTag('bff')
+        .addBearerAuth({
+            type: 'http',
+            in: 'header',
+            name: 'Authorization',
+            scheme: 'bearer',
+            bearerFormat: 'JWT'
+        })
+        .build()
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig)
+    SwaggerModule.setup(`${globalPrefix}/docs`, app, document)
+
+    const port = AppModule.CONFIGURATION.PORT
     await app.listen(port)
     Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix} `)
+    Logger.log(`🚀 Swagger documentation is running on: http://localhost:${port}/${globalPrefix}/docs `)
 
     const testConfig = AppModule.CONFIGURATION.BFF_CONFIG.TEST_CONFIG
     Logger.log(`🧪 Test Config: ${JSON.stringify(testConfig)}`)
--- libs/types/src/lib/response.dto.ts
+++ libs/types/src/lib/response.dto.ts
@@ -1,10 +1,18 @@
 import { HTTP_MESSAGE_TITLES } from '@libs/constants/http.constant'
 import { HttpStatus } from '@nestjs/common'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class ResponseDto<T> {
+    @ApiProperty({ type: 'string', example: HTTP_MESSAGE_TITLES.OK })
     title = HTTP_MESSAGE_TITLES.OK
+
+    @ApiProperty({ type: 'string', example: 'OK' })
     message?: string
+
+    @ApiProperty()
     data?: T
+
+    @ApiProperty({ type: 'number', example: HttpStatus.OK })
     statusCode = HttpStatus.OK
 
     constructor(data: Partial<ResponseDto<T>>) {
